606276ce7e0415f3b63536f51e87c6e6c14987b0
Sergey Kastryulin <snk4tr@gmail.com>
2024-07-20 17:24:00 +0300

    +

--- aggregation/nr.py
+++ aggregation/nr.py
@@ -1,4 +1,3 @@
-import os
 import torch
 import faiss
 import random
@@ -10,51 +9,6 @@ from miniball import miniball
 
 from util.io import read_json
 from util.common import value, key
-
-
-
-def get_centrorids_fr(
-    path: str, config: Dict[str, Any]
-) -> Tuple[torch.Tensor, torch.Tensor]:
-    # works for PIPAL and KADIS.
-    # Other datasets would require other processing
-
-    ratio = config["prompt_ratio"]
-    if ratio is None:
-        ratio = 1.0
-    assert 0.0 < ratio <= 1.0, f"Ratio should be in (0., 1.] interval, got {ratio}"
-
-    paths = read_json(f"{path}.json")["results_paths"]
-    embeds = torch.load(f"{path}.pt", map_location="cpu")
-
-    files_names = np.array([os.path.splitext(os.path.basename(p))[0] for p in paths])
-
-    ref_idx = []
-    ref_dist = {}
-    for idx, file in enumerate(files_names):
-        if len(file.split("_")) == 1:
-            ref_idx.append(idx)
-            ref_dist[file] = []
-        else:
-            ref_dist[file.split("_")[0]].append(idx)
-
-    ref_idx = torch.tensor(ref_idx)
-
-    ref_limit = round(len(ref_idx) * ratio)
-    print(ref_limit)
-    ref_idx_shuffled = ref_idx[np.random.permutation(len(ref_idx))]
-    ref_idx_reduced = ref_idx_shuffled[:ref_limit]
-
-    dist_idx_reduced = []
-    for idx in ref_idx_reduced:
-        dist_idx_reduced.extend(ref_dist[files_names[idx]])
-    dist_idx_reduced = torch.tensor(dist_idx_reduced)
-
-    ref_embs = torch.index_select(embeds, 0, ref_idx_reduced)
-    dist_embs = torch.index_select(embeds, 0, dist_idx_reduced)
-    ref_centroid = ref_embs.mean(dim=0)
-    dist_centroid = dist_embs.mean(dim=0)
-    return ref_centroid, dist_centroid
 
 
 def get_centroids_nr(
